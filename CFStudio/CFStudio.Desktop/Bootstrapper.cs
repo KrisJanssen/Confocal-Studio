30d69d0d330526ce7574944a81cf33f3e33629ac
Kris Janssen <kris.janssen@gmail.com>
2014-07-16 13:22:13 +0200

    Prism basics

---
+++ CFStudio/CFStudio.Desktop/Bootstrapper.cs
@@ -0,0 +1,33 @@
+﻿namespace CFStudio.Desktop
+{
+    using System.Net.Mime;
+    using System.Windows;
+
+    using Microsoft.Practices.Prism.Modularity;
+    using Microsoft.Practices.Prism.UnityExtensions;
+    using Microsoft.Practices.Unity;
+
+    /// <summary>
+    /// The bootstrapper.
+    /// </summary>
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return new Shell();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Application.Current.MainWindow = (Window)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+        }
+    }
+}
